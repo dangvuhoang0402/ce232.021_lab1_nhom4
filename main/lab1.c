c51a86eb1d631e0aabbcd824bbafd8bf61c90c6b
dang vu hoang <dangvuhoang0402@gmail.com>
2024-03-12 08:26:02 +0700

    first commit

---
+++ main/lab1.c
@@ -0,0 +1,28 @@
+/*
+ * SPDX-FileCopyrightText: 2010-2022 Espressif Systems (Shanghai) CO LTD
+ *
+ * SPDX-License-Identifier: CC0-1.0
+ */
+
+#include <stdio.h>
+#include <inttypes.h>
+#include "sdkconfig.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp_chip_info.h"
+#include "esp_flash.h"
+#include "esp_system.h"
+#include "driver/gpio.h"
+
+void app_main(void)
+{
+    gpio_set_direction(GPIO_NUM_2,GPIO_MODE_OUTPUT);
+    while(1){
+        gpio_set_level(GPIO_NUM_2, 1);
+        printf("Im on!\n");
+        vTaskDelay(100);
+        gpio_set_level(GPIO_NUM_2,0);
+        printf("Im off!\n");
+        vTaskDelay(100);
+    }
+}
